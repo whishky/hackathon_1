2d001148d8d61ec0648f49887c686d10e941bd2e
whishky <shivprasadc022@gmail.com>
2016-04-19 16:24:34 +0530

    pasword reset link added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -30,6 +34,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   #post '/events/:id/edit' => 'events#update'
 
