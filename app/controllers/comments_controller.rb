0298d8af95a9b55189486de9075ca76b257c154d
whishky <shivprasadc022@gmail.com>
2016-04-27 16:47:41 +0530

    some minor bugs in comments resolved

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,14 @@ class CommentsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
-  	#binding.pry
   	@event = Event.find(params["id"])
     @user = User.find(session[:user_id])
   	@comment = @event.comments.build(comment_params)
     @comment["user_id"] = @user["id"]
     if @comment.save
       flash[:success] = "Comment created!"
-      redirect_to root_url
+      binding.pry
+      redirect_to controller: 'events', action: 'event_info', id: @event.id
     else
       render 'events/event_info'
     end
