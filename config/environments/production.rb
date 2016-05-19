c44e2a51e4a96f6b84da0efc8e946bb516a752d3
whishky <shivprasadc022@gmail.com>
2016-05-20 04:06:47 +0530

    for mail from heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'thawing-gorge-93081.heroku.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
