4ddbb89deae90860fded9fadc3a53b48e127700b
whishky <shivprasadc022@gmail.com>
2016-05-23 14:52:51 +0530

    testing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,164 +2,136 @@ class EventsController < ApplicationController
 
 	before_action :correct_user,   only: [:edit, :update]
 
-	def start
+  def start
+  end
 
-	end
+  def index
+  end
 
-	def index
-	end
+  def new
+    if logged_in? == false
+      flash[:danger] = "please login kr"
+      redirect_to '/login'
+    end
+    @event = Event.new
+  end
 
-	def new
-		if logged_in? == false
-			flash[:danger] = "please login kr"
-			redirect_to '/login'
-		end
-		@event = Event.new
-	end
+  def create
+    @user = User.find(session[:user_id])
+    @event = @user.events.build(event_params)
+    if @event.save
+      redirect_to '/events/show'
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @event = Event.find(params[:id])
+  end
 
-	def create
-		#binding.pry
-		@user = User.find(session[:user_id])
-		@event = @user.events.build(event_params)
-		if @event.save
-			redirect_to '/events/show'
-
-		else	
-			render 'new'
-		end
-	end
-
-	def edit
-		#binding.pry
-  		@event = Event.find(params[:id])
-	end
-
-	def update
-		#binding.pry
-		@event = Event.find(params[:id])
-		#@event = Event.new(event_params)
-		#binding.pry
-
-		flg = true
-		params[:event].each do |key, value|
-			if @event.update_attribute(key , value)
-				flg &= true
-			else
-				flg &= false
-			end
-		end
-
-		if flg
-			redirect_to @event
-		else
-			redirect_to 'edit'
-		end
-
-		#if @event.save
-		#	redirect_to '/events/show'
-
-		#else	
-		#	render 'new'
-		#end
-	end
-
-	def submit_solution
-		@event = Event.find(params[:id])
-		@gallary = @event.gallaries.new
-	end
-
-	def image_upload
-		#binding.pry
-		@user = User.find(session[:user_id])
-		@event = Event.find(params[:event_id])
-  		@gallary = @event.gallaries.build(gallary_params)
-    	@gallary["user_id"] = @user["id"]
-		if @gallary.save
-			flash[:success] = "Image uploaded"
-			redirect_to controller: 'events', action: 'event_info', id: @event.id
-		else
-			flash[:danger] = "error while uploading image"
-			redirect_to controller: 'events', action: 'submit_solution', id: @event.id
-		end
-	end
-
-	def search
-		#binding.pry
-		@events = Event.search(params[:search]).order("created_at DESC")
-	end
-
-	def show
-    		#@events = Event.all
-    	#binding.pry
-    	if params[:tag]
-    		#binding.pry
-    		#tagging = Event.tagged_with(params[:tag])
-    		#events = Event.all
-    		@events = Event.joins(:taggings).where(taggings: {tag_id: Tag.find_by_name(params[:tag]).id})
-			#@events = Event.tagged_with(params[:tag])
-		else
-			@events = Event.all
-		end
-	end
-
-	def event_info
-		#binding.pry
-		@event = Event.find(params[:id])
-		@comments = @event.comments.paginate(page: params[:page])
-		@gallaries = @event.gallaries
-	end
-
-	def find_current_time
-		Time.now.strftime("%Y-%m-%d %H:%M")
-	end
-
-	def _present_events
-		#binding.pry
-		#current_date_time = Time.now.strftime("%Y-%m-%d %H:%M")
-		current_date_time = find_current_time
-		@events = Event.all
-		@events = @events.map{|k| k if k.start_date_time.to_s <= current_date_time.to_s && k.end_date_time.to_s >= current_date_time.to_s}.compact
-	end
-
-	def past_events
-		current_date_time = find_current_time
-		@events = Event.all
-		@events = @events.map{|k| k if k.end_date_time.to_s < current_date_time.to_s}.compact
-
-	end
-
-	def future_events
-		current_date_time = find_current_time
-		@events = Event.all
-		@events = @events.map{|k| k if k.start_date_time.to_s > current_date_time.to_s}.compact
-	end
-
-	# Confirms the correct user.
-    def correct_user
-      @event = Event.find(params[:id])
-      unless @event.user_id == session[:user_id]
-      	current_date_time = Time.now.strftime("%Y-%m-%d %H:%M")
-      	binding.pry
-      	if @event.start_date_time < current_date_time
-      		flash[:danger] = "you can only edit the future events"
-      		redirect_to(root_url)
-      	else
-      		flash[:danger] = "you can only edit your events"
-      		redirect_to(root_url) 
-      	end
+  def update
+    @event = Event.find(params[:id])
+    flg = true
+    params[:event].each do |key, value|
+      if @event.update_attribute(key , value)
+        flg &= true
+      else
+        flg &= false
       end
     end
+    if flg
+      redirect_to @event
+    else
+      redirect_to 'edit'
+    end
+  end
 
-	private
-		def event_params
-			params.require(:event).permit(:title, :description, :start_date_time, :end_date_time, :city, :address, :event_creater, :all_tags)
-		end
+  def submit_solution
+    @event = Event.find(params[:id])
+    @gallary = @event.gallaries.new
+  end
 
-		def event_edit_params
-			params.require(:event).permit(:title, :description, :start_date_time, :end_date_time, :city, :address, :event_creator)
-		end
+  def image_upload
+    @user = User.find(session[:user_id])
+    @event = Event.find(params[:event_id])
+    @gallary = @event.gallaries.build(gallary_params)
+    @gallary["user_id"] = @user["id"]
+    if @gallary.save
+      flash[:success] = "Image uploaded"
+      redirect_to controller: 'events', action: 'event_info', id: @event.id
+    else
+      flash[:danger] = "error while uploading image"
+      redirect_to controller: 'events', action: 'submit_solution', id: @event.id
+    end
+  end
 
-		def gallary_params
-			params.require(:gallary).permit(:image, :user_id)
-		end
+  def search
+    @events = Event.search(params[:search]).order("created_at DESC")
+  end
+
+  def show
+    if params[:tag]
+      @events = Event.joins(:taggings).where(taggings: {tag_id: Tag.find_by_name(params[:tag]).id})
+    else
+      @events = Event.all
+    end
+  end
+
+  def event_info
+    @event = Event.find(params[:id])
+    @comments = @event.comments.paginate(page: params[:page])
+    @gallaries = @event.gallaries
+  end
+
+  def find_current_time
+    Time.now.strftime("%Y-%m-%d %H:%M")
+  end
+
+  def _present_events
+    current_date_time = find_current_time
+    @events = Event.all
+    @events = @events.map{|k| k if k.start_date_time.to_s <= current_date_time.to_s && k.end_date_time.to_s >= current_date_time.to_s}.compact
+  end
+
+  def past_events
+    current_date_time = find_current_time
+    @events = Event.all
+    @events = @events.map{|k| k if k.end_date_time.to_s < current_date_time.to_s}.compact
+  end
+
+  def future_events
+    current_date_time = find_current_time
+    @events = Event.all
+    @events = @events.map{|k| k if k.start_date_time.to_s > current_date_time.to_s}.compact
+  end
+
+	# Confirms the correct user.
+  def correct_user
+    @event = Event.find(params[:id])
+    unless @event.user_id == session[:user_id]
+      current_date_time = Time.now.strftime("%Y-%m-%d %H:%M")
+      if @event.start_date_time < current_date_time
+      	flash[:danger] = "you can only edit the future events"
+      	redirect_to(root_url)
+      else
+      	flash[:danger] = "you can only edit your events"
+      	redirect_to(root_url) 
+      end
+    end
+  end
+
+  private
+    
+    def event_params
+      params.require(:event).permit(:title, :description, :start_date_time, :end_date_time, :city, :address, :event_creater, :all_tags)
+    end
+
+    def event_edit_params
+      params.require(:event).permit(:title, :description, :start_date_time, :end_date_time, :city, :address, :event_creator)
+    end
+
+    def gallary_params
+      params.require(:gallary).permit(:image, :user_id)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
 class UsersController < ApplicationController
 
-	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-	before_action :correct_user,   only: [:edit, :update]
-	before_action :admin_user,     only: :destroy
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
-	def index
-		@users = User.paginate(page: params[:page])
-	end
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
-	def show
-     @user = User.find(params[:id])
-     @events = @user.events.all()
-	end
+  def show
+    @user = User.find(params[:id])
+    @events = @user.events.all()
+  end
 
-	def new
-     @user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
   def create
      @user = User.new(user_params)
@@ -28,11 +28,11 @@ class UsersController < ApplicationController
      end
   end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	def update
+  def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Your Profile is edited"
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-	private
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
