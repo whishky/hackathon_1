3e0a4de145095f4a18713e6aae66a35ef590365d
whishky <shivprasadc022@gmail.com>
2016-05-23 13:42:40 +0530

    testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,24 +9,24 @@ class UsersController < ApplicationController
 	end
 
 	def show
-	   @user = User.find(params[:id])
-     @events = @user.events.all()
+    @user = User.find(params[:id])
+    @events = @user.events.all()
 	end
 
 	def new
-	   @user = User.new
+    @user = User.new
 	end
 
 	def create
-	   @user = User.new(user_params)
-	   if @user.save
-	   	  @user.send_activation_email
-      	flash[:info] = "Please check your email to activate your account."
-      	redirect_to root_url
-	   else
-	     	render 'new'
-	   end
-	end
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
 
 	def edit
 		@user = User.find(params[:id])
@@ -43,10 +43,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
-	   User.find(params[:id]).destroy
-	   flash[:success] = "User deleted"
-	   redirect_to users_url
-	end
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
 	private
 
