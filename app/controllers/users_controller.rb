9a2c3142112d0a86e56b6e648909bfdf994cdea9
whishky <shivprasadc022@gmail.com>
2016-05-23 13:44:39 +0530

    testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,23 +9,23 @@ class UsersController < ApplicationController
 	end
 
 	def show
-    @user = User.find(params[:id])
-    @events = @user.events.all()
+     @user = User.find(params[:id])
+     @events = @user.events.all()
 	end
 
 	def new
-    @user = User.new
+     @user = User.new
 	end
 
-	def create
-    @user = User.new(user_params)
-    if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
-    else
-      render 'new'
-    end
+  def create
+     @user = User.new(user_params)
+     if @user.save
+       @user.send_activation_email
+       flash[:info] = "Please check your email to activate your account."
+       redirect_to root_url
+     else
+       render 'new'
+     end
   end
 
 	def edit
